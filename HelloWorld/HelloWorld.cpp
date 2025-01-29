e21b887cdf3bc03257e67e61d0465e23c3fd924a
Bella P. <bellaperezppg@gmail.com>
2025-01-28 20:03:34 -0500

    Added the base
    Still need to add measures to detect all errors if the input is put incorrectly

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,12 +1,84 @@
 // HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+
+//class
+class LoginInfo
+{
+private: 
+	int ID;
+	string Username;
+	string Passwd;
+
+public:
+
+	//Constructor
+	LoginInfo (int id, const string& username, const string& passwd)
+		: ID(id), Username(username), Passwd(passwd)
+	{ }
+
+	//Login
+	bool Login(const::string& enteredUserName, const::string& enteredPassword)
+	{
+		return (Username == enteredUserName && Passwd == enteredPassword);
+	}
+
+	int getID() const 
+	{
+		return ID;
+	}
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	int ID;
+	string Username;
+	string Passwd;
+
+	cout << "Enter your ID: ";
+	cin >> ID;
+
+	cout << "Enter your Username: ";
+	cin >> Username;
+
+	cout << "Enter your Password: ";
+	cin >> Passwd;
+
+	LoginInfo account(ID, Username, Passwd);
+		while (true)
+		{
+			string enteredUserName, enteredPassword;
+
+			cout << "Enter your ID: ";
+			cin >> ID;
+
+			cout << "Enter your Username: ";
+			cin >> Username;
+
+			cout << "Enter your Password: ";
+			cin >> Passwd;
+
+			if (account.Login(Username, Passwd)) 
+			{
+				cout << "Login Sucessful\n";
+				break;
+			}
+
+			else 
+			{
+				cout << "Login failed please try again:\n";
+			}
+
+		}
+		//in c# means code was successful
+	return 0;
+};
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
